6e85321c2c22fe2a857b59188fd38a0d4b166bed
Abdur Rahim <abdurrahim300431@gmail.com>
2023-02-02 23:59:01 +0600

    Constant Variable

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+// veriable
+/*
 $name ='Abdur Rahim';
 $age = 24;
 $birth = 1998;
@@ -8,4 +10,22 @@ echo "\n";
 echo $age;
 echo "\n";
 echo "Bio {$name}, {$age}, {$birth}";
+*/
+
+$country = "My Contry Name Bangladesh";
+$city = "Thakurgaon";
+$country = "Dhaka";
+
+define ('PI', 3.1415);
+
+echo $country;
+echo "\n";
+echo $city;
+echo "\n";
+echo $country;
+echo "\n";
+
+echo "Value Of PI".PI;
+echo "\n";
+echo constant('PI');
 ?>
