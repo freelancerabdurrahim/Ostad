6334c97d9323481697ce752d05555eb024347dce
Abdur Rahim <abdurrahim300431@gmail.com>
2023-02-04 01:36:32 +0600

    addition subtraction

--- index.php
+++ index.php
@@ -2,13 +2,30 @@
 // $number = 20;
 // $number2 += 30;
 // $number3 =$number + 50;
+
 // $number -= 5;
 // $number2 = 5;
 // $division = $number / $number2;
 
-$number =50 * 2;
-$multi = (5*5) - (3*5);
+// $number =50 * 2;
+// $multi = (5*5) - (3*5);
+// $incri = 50;
+// $incri ++;
 
-$incri = 50;
-$incri ++;
-echo $incri;
+/*
+$n = 11;
+$m = $n++;
+
+$m = $n++;
+$m = $n
+$n = $n+1
+*/
+
+
+$x = 15;
+$y = ++$x;
+
+$x = $x+1
+$y = $x
+
+echo $y, "\n", $x;
