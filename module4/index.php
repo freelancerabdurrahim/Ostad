3ca806d600831d4d4b1156e674d1ed566232bac8
Abdur Rahim <abdurrahim300431@gmail.com>
2023-02-22 00:27:07 +0600

    html break

---
+++ module4/index.php
@@ -0,0 +1,4 @@
+<?php
+//$word= "Lorem Ipsum is simply dummy text \n of the printing and typesetting industry.\n Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,\n when an unknown printer took a galley of type and scrambled\n it to make a type specimen book. It has survived not only five centuries, but also the leap into\n electronic typesetting, remaining essentially unchanged.";
+//echo nl2br($word);
+echo nl2br("One line.\nAnother line.");
