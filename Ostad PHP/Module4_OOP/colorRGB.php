f0b04aa37b5f5d91fd3ae2b5560a34852111cee3
Abdur Rahim <abdurrahim300431@gmail.com>
2023-02-24 15:19:53 +0600

    RGB Color Found

--- Ostad PHP/Module4_OOP/colorRGB.php
+++ Ostad PHP/Module4_OOP/colorRGB.php
@@ -11,17 +11,37 @@ class RGB{
     function getColor(){
         return $this->color;
     }
+    public function getRGBColor(){
+        return array($this->red, $this->green, $this->blue);
+    }
+    public function redRGBColor(){
+        echo "Red = {$this->red}\nGreen = {$this->green}\nBlue = {$this->blue}";
+    }
     function setColor($colorCode){
         $this->color= ltrim( $colorCode,'#');
         $this->parseColor();
     }
     private function parseColor(){
         if($this->color){
-            $colors = sscanf($this->color,'%02x%02x%02x');
-            print_r($colors);
+            list($this->red, $this->green, $this->blue)=$colors = sscanf($this->color,'%02x%02x%02x');
+        }else{
+            list($this->red, $this->green, $this->blue)=$colors=array(0,0,0);
         }
-        
+    }
+
+    public function redRed(){
+        return ($this->red);
+    }
+
+    public function redGreen(){
+        return ($this->green);
+    }
+
+    public function redBlue(){
+        return ($this->blue);
     }
 }
 
-$myColor = new RGB("#000000");
+$myColor = new RGB("#ffffff");
+$myColor->redRGBColor();
+// echo $myColor->redBlue();
