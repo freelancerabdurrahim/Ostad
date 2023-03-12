a76a166994987c02dc630a5618ff4eb7f58d46f0
Abdur Rahim <abdurrahim300431@gmail.com>
2023-03-12 12:13:36 +0600

    Files Red

--- Module5_File_Session_Date.php/test.php
+++ Module5_File_Session_Date.php/test.php
@@ -1,3 +1,21 @@
 <?php
 
-echo "hello Word";
+$fileName= 'H:\Ostad PHP & Laravel\Module5_File_Session_Date.php\php\text.txt';
+$fp =fopen($fileName, 'r');
+while($line=fgets($fp)){
+    echo $line;
+}
+
+rewind($fp);
+while($line=fgets($fp)){
+    echo $line;
+}
+fclose($fp);
+// $line= fgets($fp);
+// echo $line;  
+
+
+$date= file($fileName);
+$data=file_get_contents($fileName);
+print_r($date);
+echo $data;
