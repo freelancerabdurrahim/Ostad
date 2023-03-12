9a6efd1fcea5ac888aa20dc0ee0dd7e5f487618f
Abdur Rahim <abdurrahim300431@gmail.com>
2023-03-13 01:26:46 +0600

    Assignment5

--- Module5_File_Session_Date.php/test.php
+++ Module5_File_Session_Date.php/test.php
@@ -1,21 +1,23 @@
 <?php
 
-$fileName= 'H:\Ostad PHP & Laravel\Module5_File_Session_Date.php\php\text.txt';
-$fp =fopen($fileName, 'r');
-while($line=fgets($fp)){
-    echo $line;
+$fileName = 'H:\Ostad PHP & Laravel\Module5_File_Session_Date.php\php\text.txt';
+if (is_readable($fileName)) {
+    $fp = fopen($fileName, 'r');
+    while ($line = fgets($fp)) {
+        echo $line;
+    }
+
+    rewind($fp);
+    while ($line = fgets($fp)) {
+        echo $line;
+    }
+    fclose($fp);
+    // $line= fgets($fp);
+    // echo $line;  
+
+
+    $date = file($fileName);
+    $data = file_get_contents($fileName);
+    print_r($date);
+    echo $data;
 }
-
-rewind($fp);
-while($line=fgets($fp)){
-    echo $line;
-}
-fclose($fp);
-// $line= fgets($fp);
-// echo $line;  
-
-
-$date= file($fileName);
-$data=file_get_contents($fileName);
-print_r($date);
-echo $data;
